fd71085753ebe6842c49c7f642c5b2d6a8efcecf
wft-dev <dev@myweb-development.com>
2023-01-04 16:45:37 +0530

    Add Responsive File

---
+++ app/utlis/index.js
@@ -0,0 +1,9 @@
+import Colors from './colors';
+import Fonts from './fonts';
+import Images from './images';
+import Constants from './constants';
+import Validate from './validate';
+import Responsive from './responsive';
+import Strings from './strings';
+import dateTimeHelper from './dateTimeHelper';
+export { Colors, Fonts, Images, Constants, Validate, Responsive,Strings, dateTimeHelper};
