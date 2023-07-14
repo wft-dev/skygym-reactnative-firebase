984b1bed0c79f63d446362ad0903abed302bdc0b
wft-dev <dev@myweb-development.com>
2023-07-14 09:44:04 +0530

    Add Colors File

---
+++ app/utils/colors.js
@@ -0,0 +1,19 @@
+
+const colors = {
+  yellow: '#FFE500',
+  yellowLight: '#CEB900',
+  yellowAlphaLight: '#E6D200',
+  yellowDark: '#AF9D00',
+  whiteLight: '#FFF8BA',
+  white: '#FFFFFF',
+  black: '#081C25',
+  dark_black: '#131313',
+  red: '#FF0404',
+  gray: '#E8E8E8',
+  grayLight: '#F4F4F4',
+  grayDim: '#C1C1C1',
+  clear: 'transparent',
+  green: '#2AD200',
+}
+
+export default colors;
